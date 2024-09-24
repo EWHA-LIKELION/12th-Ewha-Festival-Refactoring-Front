228a4ba3e6d589e9e14ad5a54bbd6f42b17015de
s-uxun <serenasol@ewha.ac.kr>
2024-09-24 12:05:28 +0900

    Init: react 설치 및 기본 구조 세팅

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import GlobalStyle from "./style/GlobalStyle";
+import "./style/normalize.css";
+
+function App() {
+  return <GlobalStyle />;
+}
+
+export default App;
