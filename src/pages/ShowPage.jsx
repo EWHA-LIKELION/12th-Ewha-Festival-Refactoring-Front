b3bd1cb2a0233770f80c958b339145748ffa8feb
haeunni <373seoha@gmail.com>
2024-10-10 16:15:32 +0900

    Move :truck: 공연 상세 경로 수정

--- src/pages/ShowPage.jsx
+++ src/pages/ShowPage.jsx
@@ -357,19 +357,12 @@ const SearchResult = styled.div`
 /* 부스 목록 */
 const BoothList = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr); /* 두 개의 열로 고정 */
+  grid-template-columns: repeat(auto-fill, minmax(170px, 170px));
   gap: 16px;
   justify-content: center;
   width: 100%;
   box-sizing: border-box;
-  grid-auto-rows: 197px;
-
-  @media (max-width: 600px) {
-    grid-template-columns: repeat(
-      2,
-      minmax(100px, 1fr)
-    ); /* 화면이 좁아지면 크기를 유동적으로 변경 */
-  }
+  grid-auto-rows: 197px; /* 높이도 설정 */
 `;
 
 /* 페이지 넘버 */
@@ -438,7 +431,7 @@ const Popup = styled.div`
   display: flex;
   justify-content: center;
   align-items: flex-end;
-  z-index: 1001;
+  z-index: 10;
 `;
 
 const PopupContent = styled.div`
